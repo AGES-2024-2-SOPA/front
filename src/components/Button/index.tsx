3282837ea44223f28697b088d41995f642e2f261
gustavo <gustavobpasquali2@gmail.com>
2024-09-09 02:24:21 -0300

    feat: componente de  botão

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
+  label: string;
+  isFluid: boolean;
+  isDisabled: boolean;
+}
+
+export default function Button(props: ButtonProps) {
+
+  const buttonStyle = {
+    backgroundColor: '#FF8E29',
+    color: '#FFFFFF',
+    width: props.isFluid ? '100%' : 'auto',
+    padding: '10px 150px',    
+    border: 'none',         
+    borderRadius: '30px',     
+  };
+
+  return (
+    <button
+      style={buttonStyle}
+      onClick={props.onClick}
+      disabled={props.isDisabled}
+    >
+      {props.label}
+    </button>
+  );
+}
