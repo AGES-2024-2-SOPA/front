edd57a01680284f72cdc4978fe9648ea22cfee8b
Eduardo Camana <camana98@gmail.com>
2024-09-12 10:49:19 -0300

    fix: Ajuste no formulario de login usando react hook form e zod

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
 
 interface InputProps {
-    type?: string;
-    icon?: string;
-    label?: string;
-    placeholder?: string;
-    onChangeCallback?: (value: string) => void;
+    type?: string,
+    icon?: string,
+    label?: string,
+    placeholder?: string,
+    register?: any;
 }
 
 const Input = ({
-                        type = 'text',
-                        icon,
-                        label,
-                        placeholder = '',
-                        onChangeCallback
-                    }:InputProps ) => {
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (onChangeCallback) {
-            onChangeCallback(event.target.value);
-        }
-    };
+                   type = 'text',
+                   icon,
+                   label,
+                   placeholder = '',
+                   register
+               }: InputProps) => {
 
     return (
         <div className="flex flex-col w-full">
@@ -28,13 +21,13 @@ const Input = ({
             <div className="relative">
                 {icon && (
                     <span className="absolute right-4 top-1/2 transform -translate-y-1/2">
-            <img src={icon} alt="icon" className="h-5 w-5" />  {/* Insere o SVG como imagem */}
+            <img src={icon} alt="icon" className="h-5 w-5"/>
           </span>
                 )}
                 <input
                     type={type}
                     placeholder={placeholder}
-                    onChange={handleChange}
+                    {...register}
                     className="w-full px-4 py-2 text-sm bg-gray-100 text-gray-900 border border-gray-300 rounded-full focus:outline-none focus:border-green-600"
                 />
             </div>
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,46 +1,60 @@
-import React, { useState } from 'react';
-import Input from '../../components/Input' // Importando o componente Input
+import { useForm } from 'react-hook-form';
+import Input from '../../components/Input';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const createUserFormSchema = z.object({
+    email: z.string()
+        .nonempty('O email é obrigatório')
+        .email('Formato de email inválido')
+        .toLowerCase(),
+    password: z.string()
+        .min(6, 'A senha precisa de 6 caracteres'),
+})
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>;
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
-    const handleEmailChange = (value: string) => {
-        setEmail(value);
-    };
+    const { register, handleSubmit, formState: { errors } } = useForm<CreateUserFormData>({
+        resolver: zodResolver(createUserFormSchema),
+    });
 
-    const handlePasswordChange = (value: string) => {
-        setPassword(value);
-    };
-
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-        console.log('Email:', email);
-        console.log('Password:', password);
+    const onSubmit = (data: any) => {
+        console.log('Dados do formulário:', data);
     };
 
     return (
         <div className="flex h-screen justify-center items-center bg-amber-50">
             <div className="flex w-[80%] h-[80%] shadow-lg rounded-lg overflow-hidden">
 
-                <div className="w-1/2 bg-white flex flex-col justify-start items-center p-10"> {/*Primeira metade*/}
+                <div className="w-1/2 bg-white flex flex-col justify-start items-center p-10"> {/*Primeira metade da tela*/}
+
                     <h1 className="text-3xl font-bold mb-16 mt-20">Login</h1>
-                    <form onSubmit={handleSubmit}
-                          className="w-full max-w-sm flex flex-col space-y-6">
+
+                    <form onSubmit={handleSubmit(onSubmit)} className="w-full max-w-sm flex flex-col space-y-6">
+
+                        {/* Campo Email */}
                         <Input
                             type="email"
                             label="Email"
                             placeholder="Digite seu email"
                             icon="/logoemail.svg"
-                            onChangeCallback={handleEmailChange}
+                            {...register("email")}
                         />
+                        {errors.email && <p className="text-red-500 text-xs mt-1">{errors.email.message}</p>} {/* Mensagem de erro */}
+
+                        {/* Campo Senha */}
                         <Input
                             type="password"
                             label="Senha"
                             placeholder="Digite sua senha"
                             icon="/logosenha.svg"
-                            onChangeCallback={handlePasswordChange}
+                            {...register("password")}
                         />
+                        {errors.password && <p className="text-red-500 text-xs mt-1">{errors.password.message}</p>} {/* Mensagem de erro */}
+
+                        {/* Botão Login */}
                         <button
                             type="submit"
                             className="w-full bg-orange-500 text-white py-2 px-4 rounded-full hover:bg-orange-600 transition mt-60 mb-2">
@@ -49,16 +63,13 @@ const Login = () => {
                     </form>
                 </div>
 
-                <div className="w-1/2 bg-orange-500 flex flex-col justify-center items-center"> {/*Segunda metade*/}
-                    <div className="p-10 rounded-lg mb-6" style={{marginTop: '100px'}}>
-                        <img src="/logoprojeto.png" alt="Logo"/>
+                <div className="w-1/2 bg-orange-500 flex flex-col justify-center items-center"> {/*Segunda metade da tela*/}
+                    <div className="p-10 rounded-lg mb-6" style={{ marginTop: '100px' }}>
+                        <img src="/logoprojeto.png" alt="Logo" />
                     </div>
                     <div className="mb-80 mt-4">
-                        <h2 className="text-2xl font-bold text-center text-white">Bem-vindo <br/> de volta!</h2>
+                        <h2 className="text-2xl font-bold text-center text-white">Bem-vindo <br /> de volta!</h2>
                     </div>
-                    {/*<div className="mt-auto mb-[260px]">*/}
-                    {/*    <h2 className="text-2xl font-bold text-left text-white -ml-80">Bem-vindo <br/> de volta!</h2>*/}
-                    {/*</div>*/}
                 </div>
             </div>
         </div>
