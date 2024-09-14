5e95c9054f00e6171c028a084405a1a349562d39
Gustavo Borges Pasquali <gustavo.pasquali@edu.pucrs.br>
2024-09-13 21:38:22 -0300

    refactor: cores padrão

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 
 const Header = ({ leftComponent, centerComponent, rightComponent }: HeaderProps) => {
   return (
-    <div className="flex justify-between items-center p-6 bg-amber-500 border-b border-gray-200">
+    <div className="flex justify-between items-center p-6 bg-accent border-border">
       <div className="flex-1 text-left">{leftComponent}</div>
       <div className="flex-1 text-center">{centerComponent}</div>
       <div className="flex-1 text-right">{rightComponent}</div>
